6ab9745d5a54941eec82a92c4b84cab0921369e8
SagredoIsmael <sagredo.ismael@live.com>
2020-04-30 00:06:18 +0200

    add options to main

--- src/redux/containers/main.js
+++ src/redux/containers/main.js
@@ -2,17 +2,20 @@ import { connect } from "react-redux"
 import { compose } from 'redux'
 import { connectNavigation } from '@sagredoismael/navigation_rn'
 import Main from '../../screens/Main'
-import { fetchMotocycles } from '../actions/motocycles'
-import { getDataMotocycles } from '../selectors/motocycles'
+import { fetchMotocycles, setSelected } from '../actions/motocycles'
+import { getDataMotocycles, isLoadingMotocycles, getErrorMotocycles } from '../selectors/motocycles'
 
 const mapStateToProps = state => (
     {
-        motocycles: getDataMotocycles(state)
+        motocycles: getDataMotocycles(state),
+        isLoading: isLoadingMotocycles(state),
+        error: getErrorMotocycles(state)
     }
 )
 
 const mapDispatchToProps ={ 
-    fetchMotocycles
+    fetchMotocycles,
+    setSelected
 }
 
 export default compose(
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,33 +1,37 @@
 import React, { useEffect } from 'react'
 import { View, Button, Text, StyleSheet, FlatList } from 'react-native'
 import { DETAIL } from '../screens/Navigator'
+import Alert from '../components/Alert'
 
-export default ({ navigate, fetchMotocycles, motocycles  }) => {
-    useEffect(() => { fetchMotocycles() }, [])
+export default ({ navigate, fetchMotocycles, motocycles, isLoading, error }) => {
+  useEffect(() => { fetchMotocycles() }, [])
+  error && Alert('Fetch error', error.message)
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={motocycles}
+        onRefresh={() => fetchMotocycles()}
+        refreshing={isLoading}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+      <Button
+        title="go to detail"
+        onPress={() => navigate(DETAIL)}
+      />
+    </View>
+  )
+}
 
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={motocycles}
-          renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
-        />
-        <Button
-          title="go to detail"
-          onPress={() => navigate(DETAIL)}
-        />
-      </View>
-    )
-  }
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center', 
-      alignItems: 'center'
-    },
-    item: {
-      padding: 10,
-      fontSize: 18,
-      height: 44,
-    },
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
