f3a9265e4863b069130eaf403daa25157d3d302e
SagredoIsmael <sagredo.ismael@live.com>
2020-04-29 22:48:34 +0200

    fix colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,5 @@
 export default {
-    primary: rgba(2, 169, 224, 1.0),
-    secondary: rgba(250, 165, 3, 1.0),
+    primary: 'rgba(2, 169, 224, 1.0)',
+    secondary: 'rgba(250, 165, 3, 1.0)',
     white: 'white'
 }
