cacce1ddd6c85236a752fa57a207aecbe373deaa
SagredoIsmael <sagredo.ismael@live.com>
2020-04-29 23:04:46 +0200

    add motocycles action

---
+++ src/redux/actions/motocycles.js
@@ -0,0 +1,10 @@
+import { SET_MOTOCYCLES } from './types'
+
+const setMotocycles = motocycles => ({
+  type: SET_MOTOCYCLES,
+  motocycles
+})
+
+export const fetchMotocycles = () => (dispatch, getState) => {
+    
+}
