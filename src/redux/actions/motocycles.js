a5cff6573e441a5805699303307b59f52b2aeaa7
SagredoIsmael <sagredo.ismael@live.com>
2020-04-29 23:23:54 +0200

    add fetch motocycles

--- src/redux/actions/motocycles.js
+++ src/redux/actions/motocycles.js
@@ -1,10 +1,34 @@
-import { SET_MOTOCYCLES } from './types'
+import { SET_MOTOCYCLES, FETCH_ERROR } from './types'
+import api from '../../utils/API'
 
 const setMotocycles = motocycles => ({
-  type: SET_MOTOCYCLES,
-  motocycles
+    type: SET_MOTOCYCLES,
+    motocycles
 })
 
-export const fetchMotocycles = () => (dispatch, getState) => {
-    
+const setError = error => ({
+    type: FETCH_ERROR,
+    error
+})
+
+export const fetchMotocycles = () => dispatch => {
+    dispatch(setMotocycles(fakeResult))
+    // fetch(api.url)
+    //     .then(json => {
+    //         dispatch(setMotocycles(json))
+    //     })
+    //     .catch(error => dispatch(setError(error)))
 }
+
+const fakeResult = [
+    {
+        "name": "John",
+        "age": 30,
+        "cars": ["Ford", "BMW", "Fiat"]
+    },
+    {
+        "name": "Isma",
+        "age": 31,
+        "cars": ["Honda", "Yamaha"]
+    },
+]
