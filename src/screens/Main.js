6829f775fdbb8a184a484b036da2fe17d0877028
SagredoIsmael <sagredo.ismael@live.com>
2020-04-30 18:19:16 +0200

    add little styles for list

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -2,18 +2,27 @@ import React, { useEffect } from 'react'
 import { View, Button, Text, StyleSheet, FlatList } from 'react-native'
 import { DETAIL } from '../screens/Navigator'
 import Alert from '../components/Alert'
+import colors from '../utils/colors'
 
 export default ({ navigate, fetchMotocycles, motocycles, isLoading, error }) => {
   useEffect(() => { fetchMotocycles() }, [])
   error && Alert('Fetch error', error.message)
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={motocycles}
-        onRefresh={() => fetchMotocycles()}
-        refreshing={isLoading}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-      />
+      {motocycles.length > 0 &&
+        <FlatList
+          data={motocycles}
+          onRefresh={() => fetchMotocycles()}
+          refreshing={isLoading}
+          renderItem={({ item }) =>
+            <View
+              style={styles.item}>
+              <Text style={styles.textItem}>{item.id}</Text>
+              <Text>{item.modelo} - {item.nombre}</Text>
+            </View>
+          }
+        />}
       <Button
         title="go to detail"
         onPress={() => navigate(DETAIL)}
@@ -25,13 +34,15 @@ export default ({ navigate, fetchMotocycles, motocycles, isLoading, error }) => 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: 40,
   },
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    borderBottomWidth: 2,
+    borderBottomColor: colors.primary,
+    padding: 10
+  },
+  textItem: {
+    fontWeight: 'bold',
+    fontSize: 18
   },
 })
